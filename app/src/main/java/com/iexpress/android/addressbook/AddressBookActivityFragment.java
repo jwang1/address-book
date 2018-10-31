b85a36823154c85ce3ed01d810a84015006a02fb
jwang1 <sigkdd@yahoo.com>
2018-10-30 23:35:59 -0400

    init checkin

---
+++ app/src/main/java/com/iexpress/android/addressbook/AddressBookActivityFragment.java
@@ -0,0 +1,22 @@
+package com.iexpress.android.addressbook;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AddressBookActivityFragment extends Fragment {
+
+    public AddressBookActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_address_book, container, false);
+    }
+}
